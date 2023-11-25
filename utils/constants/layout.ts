e0ef044b24a886d83c121ad532d617a2ac36ba04
chihuong <chihuong8a1@gmail.com>
2023-11-25 20:24:04 +0700

    View All Item

--- utils/constants/layout.ts
+++ utils/constants/layout.ts
@@ -36,7 +36,7 @@ export const PAGE_MENU_MOD = [
   { id: 5, to: '/admin/user', icon: 'user', label: 'Danh sách người dùng', parentId: 2 },
   { id: 6, to: '/admin/category', icon: 'category', label: 'Thể loại', hideToggled: true },
   { id: 7, icon: 'product', label: 'Sản phẩm'},
-  { id: 8, to: '/expert/items', icon: 'list-product', label: 'Danh sách Sản phẩm', parentId: 7 },
+  { id: 8, to: '/admin/item', icon: 'list-product', label: 'Danh sách Sản phẩm', parentId: 7 },
   { id: 9,  icon: 'auction', label: 'Phiên đấu giá' },
   { id: 10, to: '/admin/auction', icon: 'regulatory', label: 'Danh sách', parentId: 9 },
   { id: 11, to: '/admin/auction/create', icon: 'new-page', label: 'Tạo mới', parentId: 9 },
